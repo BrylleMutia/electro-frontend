0487427a0c6168c8026e43241c22a20f0e5907fb
Brylle Mutia <bryllemutia3@gmail.com>
2021-04-02 14:27:24 +0800

    updated showcase carousel / images

--- src/components/Showcase/index.tsx
+++ src/components/Showcase/index.tsx
@@ -2,50 +2,32 @@ import React, { useState } from "react";
 import styles from "./Showcase.module.scss";
 
 import { Carousel } from 'antd';
-import { Button } from "@material-ui/core";
 
 import Img1 from "./assets/1.png";
 import Img2 from "./assets/2.png";
 import Img3 from "./assets/3.png";
 
-const Showcase = () => {
-  const [items] = useState([
+function Showcase() {
+  const items = [
     {
       product: "Samsung Galaxy Note 20",
-      title: "The New Standard",
-      price: '39, 999.00',
       image: Img1
     },
     {
-      name: "MacBook Air",
-      description: "Lightweight Tech",
-      price: "54, 990.00",
+      product: "MacBook Air",
       image: Img2
     },
     {
-      name: "Google Pixel 5",
-      description: "The Ultimate Phone",
-      price: "36, 854.99",
+      product: "Google Pixel 5",
       image: Img3
     },
-  ]);
+  ];
 
   return (
     <Carousel className={styles.carousel}>
       {items.map((item) => (
         <div className={styles.carousel_item}>
-          <div>
-            <h4>{item.title}</h4>
-            <h3>{item.description}</h3>
-            <p>
-              P {item.price}
-            </p>
-            <Button color="primary" variant="contained">
-              Buy Now
-            </Button>
-          </div>
-
-          <img src={item.image} alt={item.title} />
+          <img src={item.image} alt={item.product} />
         </div>
       ))}
     </Carousel>
