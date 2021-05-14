b7e4fcb7482c0d08bd814b63f4e0caeb308790f0
Brylle Mutia <bryllemutia3@gmail.com>
2021-05-14 15:32:19 +0800

    moved styled-breadcrumbs to styled-components

--- src/components/ProductNav/index.tsx
+++ src/components/ProductNav/index.tsx
@@ -1,30 +1,15 @@
 import React from "react";
-import { emphasize, withStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../../redux/hooks";
 
+import { StyledBreadcrumb } from "../StyledComponents"
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
-import Chip from "@material-ui/core/Chip";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import HomeIcon from "@material-ui/icons/Home";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-const StyledBreadcrumb = withStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.grey[100],
-    height: theme.spacing(3),
-    color: theme.palette.grey[800],
-    fontWeight: theme.typography.fontWeightRegular,
-    "&:hover, &:focus": {
-      backgroundColor: theme.palette.grey[300],
-    },
-    "&:active": {
-      boxShadow: theme.shadows[1],
-      backgroundColor: emphasize(theme.palette.grey[300], 0.12),
-    },
-  },
-}))(Chip);
+
 
 function ProductNav() {
   let { currentProduct } = useAppSelector((state) => state.shop);
--- src/components/ProductTabs/ReviewsTab/index.tsx
+++ src/components/ProductTabs/ReviewsTab/index.tsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import styles from "./ReviewsTab.module.scss";
+import { useAppSelector } from "../../../redux/hooks";
+
+import Review from "./Review";
+import RatingBar from "./Review/RatingBar";
 
 function ReviewsTab() {
+  const { reviews } = useAppSelector((state) => state.shop.currentProduct);
+
+  const getAverageRating = () => {
+    if (reviews) {
+      const totalRating = reviews?.reduce((total, current) => (total = total + current.rating), 0);
+      return (totalRating / reviews?.length).toPrecision(2);
+    }
+  };
+
+  const getRatingPercentages = () => {
+    // get occurence percentage of each rating on all reviews
+    // data needed for rating bar
+
+    let ratingPercentages = [];
+
+    for (let i = 1; i <= 5; i++) {
+      let totalOccurrence = 0;
+
+      reviews?.forEach((review) => {
+        if (review.rating == i) totalOccurrence++;
+      });
+      ratingPercentages.push(getPercentage(totalOccurrence));
+    }
+
+    // reverse the array bc ratings must render in descending order
+    return ratingPercentages.reverse();
+  };
+
+  const getPercentage = (ratingOccurence: number) => {
+    // percentage of current rating from the overall number of ratings
+    // ex. product may have 80% 4-star ratings and 20% 3-star.
+
+    if (reviews?.length) {
+      return (ratingOccurence / reviews?.length) * 100;
+    } else return 0;
+  };
+
+  useEffect(() => {
+    getRatingPercentages();
+  }, []);
+
+
+  if (!reviews?.length) return <h4>No reviews</h4>;
+
   return (
     <div>
-      Reviews
+      <h5>Based on {reviews?.length} reviews</h5>
+
+      <div className={styles.rating_container}>
+        <div className={styles.rating}>{getAverageRating()}</div>
+        <div className={styles.overall}>Overall</div>
+      </div>
+
+      {getRatingPercentages().map((ratingPercentage, index) => (
+        <RatingBar rating={5 - index} key={index} percentage={ratingPercentage} />
+      ))}
+
+      {reviews?.map((review, index) => (
+        <Review reviewDetails={review} key={index} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default ReviewsTab
+export default ReviewsTab;
