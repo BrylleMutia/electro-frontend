f10d4fd2d66d5011b70bc30ade0d62cdb32dd79f
Brylle Mutia <bryllemutia3@gmail.com>
2021-05-15 13:54:01 +0800

    added user review component

--- src/components/ProductTabs/ReviewsTab/Review/index.tsx
+++ src/components/ProductTabs/ReviewsTab/Review/index.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { ReviewInterface } from "../../../../redux/types"
+import React from "react";
+import styles from "./Review.module.scss"
+import { ReviewInterface } from "../../../../redux/types";
+
+import Rating from "@material-ui/lab/Rating";
+import Avatar from "@material-ui/core/Avatar";
 
 interface Props {
-  reviewDetails: ReviewInterface
+  reviewDetails: ReviewInterface;
 }
 
 const Review: React.FC<Props> = ({ reviewDetails }) => {
-  const { feedback } = reviewDetails;
+  const { feedback, rating, user, updated_at } = reviewDetails;
 
   return (
-    <div>
-      {feedback}
+    <div className={styles.review}>
+      <Rating size="small" className={styles.rating} name="disabled" value={rating} readOnly />
+      <p>{feedback}</p>
+      <div className={styles.user_details}>
+        <Avatar className={styles.avatar} src={user.image} alt={user.name} sizes="10px" />
+        <h4>{user.name}</h4>
+        <h5>{updated_at.slice(0, 10)}</h5>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Review
+export default Review;
