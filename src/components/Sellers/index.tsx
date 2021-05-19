7eee96ff81ae78606431c72c801caca56458da64
Brylle Mutia <bryllemutia3@gmail.com>
2021-05-19 10:58:50 +0800

    removed indicator in sellers tab

--- src/components/Sellers/index.tsx
+++ src/components/Sellers/index.tsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Sellers.module.scss";
 import { useAppSelector } from "../../redux/hooks";
+
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 
 export default function Sellers() {
   const { sellers } = useAppSelector((state) => state.shop);
+  const [tabView, setTabView] = useState(0);
+
+  const handleTabChange = (e: React.ChangeEvent<{}>, tabIndex: number) => {
+    setTabView(tabIndex);
+  };
 
   return (
     <section>
       <div className={styles.sellers_header}>
         <Typography>Sellers</Typography>
       </div>
-      <Tabs indicatorColor="primary" textColor="primary" variant="scrollable" scrollButtons="auto" aria-label="scrollable auto tabs example">
+      <Tabs
+        value={tabView}
+        onChange={handleTabChange}
+        TabIndicatorProps={{
+          style: {
+            backgroundColor: "transparent",
+          },
+        }}
+        textColor="primary"
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="scrollable auto tabs example">
         {sellers?.map((seller, index) => (
           <Tab key={index} icon={<img src={seller.image} style={{ width: "80px" }} />} />
         ))}
