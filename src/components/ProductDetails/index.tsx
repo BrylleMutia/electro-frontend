91ed49719736cf6fc4cc69f45692a63532e434f4
Brylle Mutia <bryllemutia3@gmail.com>
2021-05-28 11:03:37 +0800

    added loading skeleton to product details page

--- src/components/ProductDetails/index.tsx
+++ src/components/ProductDetails/index.tsx
@@ -6,28 +6,39 @@ import cx from "classnames";
 import AddToCartForm from "./AddToCartForm";
 import Chip from "@material-ui/core/Chip";
 import Rating from "@material-ui/lab/Rating";
+import Skeleton from "@material-ui/lab/Skeleton";
 
 function ProductDetails() {
-  let { currentProduct } = useAppSelector((state) => state.shop);
+  let { currentProduct, isLoading } = useAppSelector((state) => state.shop);
 
   return (
     <div className={styles.product_container}>
       <div className={styles.product_image}>
-        <img src={currentProduct?.product_image} alt="product" />
+        {isLoading ? (
+          <Skeleton>
+            <img src={currentProduct?.product_image} alt="product" />
+          </Skeleton>
+        ) : (
+          <img src={currentProduct?.product_image} alt="product" />
+        )}
       </div>
 
       <div className={styles.details}>
         <div className={cx(styles.categories, styles.mb_sm)}>
-          {currentProduct?.categories.map((category, index) => (
-            <Chip size="small" key={index} label={category.name} />
-          ))}
+          {currentProduct?.categories.map((category, index) =>
+            isLoading ? (
+              <Skeleton>
+                <Chip size="small" key={index} label={category.name} />
+              </Skeleton>
+            ) : (
+              <Chip size="small" key={index} label={category.name} />
+            )
+          )}
         </div>
-        <h2>{currentProduct?.product_name}</h2>
-        <h4>{currentProduct?.seller.name}</h4>
-        <div className={styles.mb_sm}>
-          <Rating name="disabled" value={3} readOnly />
-        </div>
-        <p className={styles.description}>{currentProduct?.description}</p>
+        <h2>{isLoading ? <Skeleton /> : currentProduct?.product_name}</h2>
+        <h4>{isLoading ? <Skeleton /> : currentProduct?.seller.name}</h4>
+        <div className={styles.mb_sm}>{isLoading ? <Skeleton /> : <Rating name="disabled" value={3} readOnly />}</div>
+        <p className={styles.description}>{isLoading ? <Skeleton height="10em" /> : currentProduct?.description}</p>
       </div>
 
       <div className={styles.cart_form}>
