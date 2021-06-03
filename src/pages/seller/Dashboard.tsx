8199d786de5338d9ef7942305b1230ce5930f37f
Brylle Mutia <bryllemutia3@gmail.com>
2021-06-03 15:52:00 +0800

    added seller dashboard

--- src/pages/seller/Dashboard.tsx
+++ src/pages/seller/Dashboard.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import styles from "./Seller.module.scss";
+import { useAppDispatch } from "../../redux/hooks";
+import { getAllProducts } from "../../redux/shop/shopSlice";
+import { Link } from "react-router-dom";
+
+import Button from "@material-ui/core/Button";
 
 function Dashboard() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getAllProducts(0));
+  }, []);
+
   return (
-    <main>
-      Dashboard
+    <main className={styles.container}>
+      <div className={styles.mt_xl}>
+        <Button component={Link} to="/product/new" variant="contained" disableElevation color="primary">
+          Add New Product
+        </Button>
+      </div>
     </main>
-  )
+  );
 }
 
 export default Dashboard;
