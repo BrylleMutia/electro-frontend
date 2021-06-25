338b81effa4475dab368693b58ab2944f3555d30
Brylle Mutia <bryllemutia3@gmail.com>
2021-06-25 10:27:52 +0800

    added capitalize filter

--- src/utils/filters.ts
+++ src/utils/filters.ts
@@ -1,3 +1,7 @@
 export const numWithCommas = (price: number | string) => {
   return price.toLocaleString().replace(",", ", ");
 }
+
+export const capitalize = (word: string) => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
